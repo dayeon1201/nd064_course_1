f077910a7ff3d316aa4f282c5f1d593f4bfae58d
dayeon1201 <dydid1201@gmail.com>
2024-08-01 11:06:32 +0700

    Add the health check endpoint at app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -65,6 +65,14 @@ def create():
 
     return render_template('create.html')
 
+#add the healthcheck endpoint
+@app.route('/healthz')
+def healthz():
+    response = {
+        "result": "OK - healthy"
+    }
+    return jsonify(response), 200
+
 # start the application on port 3111
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port='3111')
